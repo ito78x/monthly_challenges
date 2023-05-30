75027aecbeeb9404efae1c728f7e30331c7e0c14
ito78x <127704051+ito78x@users.noreply.github.com>
2023-05-29 22:40:29 -0300

    Dynamic URLs

--- challenges/views.py
+++ challenges/views.py
@@ -20,16 +20,11 @@ monthly_challenges = {
 }
 
 def index(request):
-  list_items = ""
   months = list(monthly_challenges.keys())
 
-  for month in months:
-    capitalized_month = month.capitalize()
-    month_path = reverse("month-challenge", args=[month])
-    list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-  response_data = f"<ul>{list_items}</ul>"
-  return HttpResponse(response_data)  
+  return render(request, "challenges/index.html", {
+    "months": months
+  })
 
 def monthly_challenge_by_number(request, month):
   months = list(monthly_challenges.keys())
@@ -44,7 +39,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
-    return render(request, "challenges/challenge.html")
+    return render(request, "challenges/challenge.html", {
+      "text": challenge_text,
+      "month_name": month.capitalize()
+    })
   except:  
     return HttpResponseNotFound("This month is not supported!") 
   
